62b67db8d2ce99c349d7a91e28962ab9a7c71392
precydkn <diokno.precy@gmail.com>
2025-01-15 02:15:43 +0400

    Update index.js

--- Frontend/index.js
+++ Frontend/index.js
@@ -7,125 +7,127 @@ function Lyrics() {
         span.addEventListener('mousemove', (event) => toEnglish(event, span));
         span.addEventListener('mouseleave', toKorean);
     });
-// tooltip func for floating lyrics translation
-function toEnglish(event, l) {
-    // creating tooltip
-    let tooltip = document.querySelector('#tooltip');
-    if (!tooltip) {
-        tooltip = document.createElement('div');
-        tooltip.id = 'tooltip';
-        tooltip.style.position = 'absolute';
-        tooltip.style.padding = '10px';
-        tooltip.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
-        tooltip.style.color = '#fff';
-        tooltip.style.borderRadius = '8px';
-        tooltip.style.fontSize = '26px';
-        tooltip.style.pointerEvents = 'none';
-        tooltip.style.zIndex = '1000';
-        document.body.appendChild(tooltip);
-    }
 
-    // Set the tooltip content based on the hovered element's ID
-    let engText;
-    switch (l.id) {
-        case 'l1':
-            engText = `
-            This is just what I’m like ever since that day<br>
-            Can’t even pick up the guitar anymore<br>
-            Yeah, it’s just what I’m like ever since that day<br>
-            It’s funny but
-            `;
-            break;
+    // tooltip func for floating lyrics translation
+    function toEnglish(event, l) {
+        
+        // creating tooltip
+        let tooltip = document.querySelector('#tooltip');
+        if (!tooltip) {
+            tooltip = document.createElement('div');
+            tooltip.id = 'tooltip';
+            tooltip.style.position = 'absolute';
+            tooltip.style.padding = '10px';
+            tooltip.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+            tooltip.style.color = '#fff';
+            tooltip.style.borderRadius = '8px';
+            tooltip.style.fontSize = '26px';
+            tooltip.style.pointerEvents = 'none';
+            tooltip.style.zIndex = '1000';
+            document.body.appendChild(tooltip);
+        }
 
-        case 'l2':
-            engText = `
-            Dirty laundry's overflowing in the washer<br>
-            Hair's sticking up, it's a mess<br>
-            Outside's bustling, even cherry blossoms look busy but not me<br>
-            It's just pathetic
-            `;
-            break;
+        // lyrics eng translations per id
+        let engText;
+        switch (l.id) {
+            case 'l1':
+                engText = `
+                This is just what I’m like ever since that day<br>
+                Can’t even pick up the guitar anymore<br>
+                Yeah, it’s just what I’m like ever since that day<br>
+                It’s funny but
+                `;
+                break;
 
-        case 'l3':
-            engText = `
-            It's just pathetic<br>
-            Yeah, I've got it bad<br>
-            It's not like tomorrow<br>
-            I'll wake up as a brand new person<br>
-            And to use my memories<br>
-            To write another song<br>
-            There's nothing I hate more than that
-            `;
-            break;
+            case 'l2':
+                engText = `
+                Dirty laundry's overflowing in the washer<br>
+                Hair's sticking up, it's a mess<br>
+                Outside's bustling, even cherry blossoms look busy but not me<br>
+                It's just pathetic
+                `;
+                break;
 
-        case 'l4':
-            engText = `
-            This is just what I'm like ever since that day<br>
-            Can't even pick up the guitar anymore<br>
-            Yeah, it's just what I'm like ever since that day<br>
-            It's funny but<br>
-            If I say I love you I love you I love you just for today<br>
-            Knowing I'd regret it, would it be better that way?<br>
-            Love you
-            `;
-            break;
+            case 'l3':
+                engText = `
+                It's just pathetic<br>
+                Yeah, I've got it bad<br>
+                It's not like tomorrow<br>
+                I'll wake up as a brand new person<br>
+                And to use my memories<br>
+                To write another song<br>
+                There's nothing I hate more than that
+                `;
+                break;
 
-        case 'l6':
-            engText = `
-            Wanna tell ya<br>
-            I could fake the inspiration<br>
-            And write a lyric that's so cliché but<br>
-            In the end, it all comes back to you<br>
-            So sick of being sober<br>
-            Pretending this isn't a song about you<br>
-            I'll just keep on living like that, today, tomorrow, on and on<br>
-            But I'm so sick of it
-            `;
-            break;
+            case 'l4':
+                engText = `
+                This is just what I'm like ever since that day<br>
+                Can't even pick up the guitar anymore<br>
+                Yeah, it's just what I'm like ever since that day<br>
+                It's funny but<br>
+                If I say I love you I love you I love you just for today<br>
+                Knowing I'd regret it, would it be better that way?<br>
+                Love you
+                `;
+                break;
 
-        case 'l7':
-            engText = `
-            And to use my memories<br>
-            To write another song<br>
-            There’s nothing I hate more than that
-            `;
-            break;
+            case 'l6':
+                engText = `
+                Wanna tell ya<br>
+                I could fake the inspiration<br>
+                And write a lyric that's so cliché but<br>
+                In the end, it all comes back to you<br>
+                So sick of being sober<br>
+                Pretending this isn't a song about you<br>
+                I'll just keep on living like that, today, tomorrow, on and on<br>
+                But I'm so sick of it
+                `;
+                break;
 
-        case 'l8':
-            engText = `
-            This is just what I'm like ever since that day<br>
-            Can't even pick up the guitar anymore<br>
-            Yeah, it's just what I'm like ever since that day<br>
-            It's funny but<br>
-            If I say I love you I love you I love you just for today<br>
-            Knowing I'd regret it, would it be better that way?<br>
-            Love you
-            `;
-            break;
+            case 'l7':
+                engText = `
+                And to use my memories<br>
+                To write another song<br>
+                There’s nothing I hate more than that
+                `;
+                break;
 
-        case 'l9':
-            engText = `
-            To be honest, music is nothing but<br>
-            One big emotional black hole<br>
-            Writing songs, playing guitar, it all feels meaningless<br>
-            Like I'm forcing a feeling, it all feels phony<br>
-            So I'm done<br>
-            What’s it matter anyway
-            `;
-            break;
+            case 'l8':
+                engText = `
+                This is just what I'm like ever since that day<br>
+                Can't even pick up the guitar anymore<br>
+                Yeah, it's just what I'm like ever since that day<br>
+                It's funny but<br>
+                If I say I love you I love you I love you just for today<br>
+                Knowing I'd regret it, would it be better that way?<br>
+                Love you
+                `;
+                break;
 
-        case 'l10':
-            engText = `
-            (I love you)<br>
-            Dum dum dum yeah<br>
-            (I love you I love you)<br>
-            Dum dum dum yeah<br>
-            `;
-            break;
+            case 'l9':
+                engText = `
+                To be honest, music is nothing but<br>
+                One big emotional black hole<br>
+                Writing songs, playing guitar, it all feels meaningless<br>
+                Like I'm forcing a feeling, it all feels phony<br>
+                So I'm done<br>
+                What’s it matter anyway
+                `;
+                break;
 
-        default:
-            engText = null;
-    }
+            case 'l10':
+                engText = `
+                (I love you)<br>
+                Dum dum dum yeah<br>
+                (I love you I love you)<br>
+                Dum dum dum yeah<br>
+                `;
+                break;
+
+            default:
+                engText = null;
+        }
 
     if (engText == null) tooltip.style.display = none;
     else tooltip.innerHTML = engText;
