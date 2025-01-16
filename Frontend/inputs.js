454391acf5bb527dddecda58371ba4b83dbfd191
precydkn <diokno.precy@gmail.com>
2025-01-16 22:40:20 +0400

    Update inputs.js

--- Frontend/inputs.js
+++ Frontend/inputs.js
@@ -1,31 +1,3 @@
-//==========SIGN-IN==========
-const signupBtn = document.getElementById("signup-btn");
-signupBtn.addEventListener("click", async () => {
-    const email = document.getElementById("signup-email").value;
-    const username = document.getElementById("signup-username").value;
-    const password = document.getElementById("signup-password").value;
-    const alertMessage = document.getElementById("alert-p");
-
-    const response = await fetch("http://localhost:4000/api/user", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, username, password }),
-    });
-
-    const data = await response.json();
-    if (response.ok) {
-        console.log("User created:", data);
-        alertMessage.style.color = '#299CF9';
-        alertMessage.innerHTML = "Welcome! You may now log in.";
-    } else {
-        console.error("Error:", data);
-        alertMessage.style.color = 'red';
-        alertMessage.innerHTML = "Error:", data;
-    }
-});
-
 //==========LOGIN==========
 const loginBtn = document.getElementById("login-btn"); //get login button
 const alertMessage = document.getElementById("alert-p"); //get p tag with #alert
@@ -45,11 +17,42 @@ loginBtn.addEventListener("click", async () => {
     const data = await response.json();
     if (response.ok) {
         console.log("Login successful:", data);
+        alertMessage.style.display = 'block';
         alertMessage.style.color = '#299CF9';
-        alertMessage.innerHTML = "Welcome! You are now logged in.";
+        alertMessage.innerHTML = "Hello! You are now logged in.";
     } else {
         console.error("Login failed:", data);
+        alertMessage.style.display = 'block';
         alertMessage.style.color = 'red';
         alertMessage.innerHTML = "Login failed:", data;
+    }
+});
+
+
+//==========SIGN-IN==========
+const signupBtn = document.getElementById("signup-btn");
+signupBtn.addEventListener("click", async () => {
+    const email = document.getElementById("signup-email").value;
+    const username = document.getElementById("signup-username").value;
+    const password = document.getElementById("signup-password").value;
+    const alertMessage = document.getElementById("alert-p");
+
+    const response = await fetch("http://localhost:4000/api/user", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, username, password }),
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+        alertMessage.style.display = 'block';
+        alertMessage.style.color = '#299CF9';
+        alertMessage.innerHTML = `Welcome ${username}! You may now log in.`;
+    } else {
+        alertMessage.style.display = 'block';
+        alertMessage.style.color = 'red';
+        alertMessage.innerHTML = "Error:", data;
     }
 });
